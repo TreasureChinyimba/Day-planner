fdf839d6604e3daa411744d0f656208b784b7ba6
Treasure Chinyimba <Treasurearthur4@gmail.com>
2023-12-05 21:57:03 +0200

    added the input box to display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
   return (
     <div className="App">
-      
-          <h1>To Do List</h1>
-      
+      <h1>To Do List</h1>
+
+      {/* Form to add new tasks */}
+      <form onSubmit={(e) => { e.preventDefault(); addTask(); }}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button type="submit">Add Task</button>
+      </form>
+
+      {/* Display tasks here */}
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
